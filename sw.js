77fa1e6c14a3df058d50be6a9a2c3b54da50d4db
EnotTail <darkenott@gmail.com>
2026-07-13 03:44:15 +0300

    Raccoon Training

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'enot-fitness-v1';
+const ASSETS = [
+  '/PWA-app/',
+  '/PWA-app/index.html',
+  '/PWA-app/manifest.json',
+  '/PWA-app/icons/icon-192.png',
+  '/PWA-app/icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
